88b7d5d2f41acd1febb449937d0c072c0464293f
sai <sai@5thdt.com>
2021-05-13 10:42:30 +0530

    Content added in pages resources journals section

---
+++ assets/js/cart/cart.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+document.querySelectorAll('.add-to-cart').forEach(button => {
+    button.addEventListener('click', e => {
+        button.classList.toggle('added');
+    });
+});
